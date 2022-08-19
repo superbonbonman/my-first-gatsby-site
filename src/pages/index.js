ce116d85f743648d0b0f713960b2701a8309e1ef
superbonbonman <111265168+superbonbonman@users.noreply.github.com>
2022-08-19 01:58:13 -0400

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial</p>
+      <StaticImage
+        alt="Crossover fan art shows characters from the Stein's;Gate anime series styled after the classic film, Back to the Future, drinking Dr. Pepper near the Delorean Time Machine"
+        src="../images/future_gadget_gate_img1.jpg"
+      />
     </Layout>
   )
 }
